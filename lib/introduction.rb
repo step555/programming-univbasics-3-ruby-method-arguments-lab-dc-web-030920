2fb19c322a0866542b6577a5982cc96daa5160d1
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-16 03:21:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,9 +9,11 @@ end
 
 introduction("Pete")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
+introduction_with_language("Norris", "French")
 
 
 
